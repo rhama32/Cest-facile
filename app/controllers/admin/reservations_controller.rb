809c3df859ae59e31d60287ba51b843de162f722
rhama32 <r.hamaoka32915@gmail.com>
2023-01-13 06:36:59 +0000

    admin/reservations編集追加

--- app/controllers/admin/reservations_controller.rb
+++ app/controllers/admin/reservations_controller.rb
@@ -1,9 +1,15 @@
 class Admin::ReservationsController < ApplicationController
 
  def index
+  @reservations = Reservation.all
+  @admin_customers = Customer.all
+  @hospitals = Hospital.all
  end
 
  def show
+  @reservation = Reservation.find(params[:id])
+  @admin_customer = Customer.find(params[:id])
+  @hospital = Hospital.find(params[:id])
  end
 
  def update
