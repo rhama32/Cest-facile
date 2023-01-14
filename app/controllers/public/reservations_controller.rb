d8e05e8e23a44f0e50d017753ce422f8133f8671
rhama32 <r.hamaoka32915@gmail.com>
2023-01-14 06:50:15 +0000

    public/reservations

--- app/controllers/public/reservations_controller.rb
+++ app/controllers/public/reservations_controller.rb
@@ -1,23 +1,32 @@
 class Public::ReservationsController < ApplicationController
 
  def new
-  @reservations = Reservation.all
-  @public_customers = current_customer
-  @hospitals = Hospital.all
+  @reservation = Reservation.new
+  @hospital = Hospital.all
  end
 
  def confirm
  end
 
- def complete
- end
+
 
  def create
+  @reservation = Reservation.new
+  if @reservation.save
+   flash[:notice] = "予約の受付が完了しました。"
+   redirect_to public_reservations_path
  end
+end
 
  def index
+  @reservations = Reservation.all
+  @public_customers = Customer.all
+  @hospitals = Hospital.all
  end
 
  def show
+  @reservations = Reservation.find(params[:id])
+  @public_customers = current_customer
+  @hospitals = Hospital.find(params[:id])
  end
 end
