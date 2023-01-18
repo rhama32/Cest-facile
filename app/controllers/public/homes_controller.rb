c6de2b73ec6879892f056b321fd79804833082f9
rhama32 <r.hamaoka32915@gmail.com>
2023-01-18 09:00:51 +0000

    定期コミット

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,15 @@
 class Public::HomesController < ApplicationController
 
 def top
+  @genres = Genre.all
+  @hospitals = Hospital.all
+  if params[:genre_id]
+    @genre = Genre.find(params[:genre_id])
+    @hospitals = @genre.hospitals
+  elsif @search_hospitals
+    @hospitals = @search_hospitals.page(params[:page])
+    @hospitals_count = @search_hospitals.all.count
+  end
 end
 
 def about
