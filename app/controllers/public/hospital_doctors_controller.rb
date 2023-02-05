3eb3e482a92b04dfce931b851565ea09988ceb89
rhama32 <r.hamaoka32915@gmail.com>
2023-02-05 13:02:36 +0000

    public/hospital_doctors

--- app/controllers/public/hospital_doctors_controller.rb
+++ app/controllers/public/hospital_doctors_controller.rb
@@ -1,3 +1,13 @@
 class Public::HospitalDoctorsController < ApplicationController
  before_action :authenticate_customer!
+ 
+ def index
+  @hospital_doctors = Doctor.all
+ end
+ 
+ def show
+  @hospital_doctor = Doctor.find(params[:id])
+  @hospital = Hospital.find_by(id: @hospital_doctor.hospital_id)
+ end
+ 
 end
