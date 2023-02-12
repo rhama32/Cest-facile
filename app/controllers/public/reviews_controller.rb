be63de10062d40377bf118e421e7d727f77d8688
rhama32 <r.hamaoka32915@gmail.com>
2023-02-12 04:39:26 +0000

    下書き

--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -38,24 +38,24 @@ def update
     @review = Review.find(params[:id])
     # ①下書きレビューの更新（公開）の場合
     if params[:publicize_draft]
-      # レシピ公開時にバリデーションを実施
+      # レビュー公開時にバリデーションを実施
       # updateメソッドにはcontextが使用できないため、公開処理にはattributesとsaveメソッドを使用する
       @review.attributes = review_params.merge(is_draft: false)
       if @review.save
-        redirect_to public_reviews_path(@review.id), notice: "下書きを公開しました。"
+        redirect_to public_reviews_path(@review.id), notice: "下書きをレビュー公開しました。"
       else
         @review.is_draft = true
-        render :edit, alert: "レビューを公開できませんでした。"
+        render :edit, alert: "下書きを公開できませんでした。"
       end
     # ②公開済みレビューの更新の場合
     elsif params[:update_post]
       @review.attributes = public_reviews_path
       if @review.save
-        redirect_to public_reviews_path(@review.id), notice: "下書きレビューを更新しました。"
+        redirect_to public_reviews_path(@review.id), notice: "公開済みのレビューを更新しました。"
       else
-        render :edit, alert: "レビューを更新できませんでした。"
+        render :edit, alert: "公開済みレビューを編集できませんでした。"
       end
-    # ③下書きレシピの更新（非公開）の場合
+    # ③下書きレビューの更新（非公開）の場合
     else
       if @review.update(review_params)
         redirect_to public_reviews_path(@review.id), notice: "下書きレビューを更新しました。"
@@ -82,7 +82,7 @@ def update
  def destroy
   @review = Review.find(params[:id])
      if @review.destroy
-  redirect_to public_reviews_path
+  redirect_to public_reviews_path, notice: "レビューを削除しました。"
    end
  end
 
