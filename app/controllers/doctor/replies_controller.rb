75735bc990795ecd0cf045915a9551cd58b343cc
rhama32 <r.hamaoka32915@gmail.com>
2023-02-12 11:32:51 +0000

    modify10

--- app/controllers/doctor/replies_controller.rb
+++ app/controllers/doctor/replies_controller.rb
@@ -10,7 +10,7 @@ class Doctor::RepliesController < ApplicationController
      @review = Review.find(params[:review_id])
      @reply = current_doctor.replies.build(reply_params)
      if @reply.save
-       flash[:notice] = "返信が完了しました."
+       flash[:notice] = "返信が完了しました。"
        redirect_to doctor_reviews_path
      else
       render :index, flash[:alret] = "返信に失敗しました。"
