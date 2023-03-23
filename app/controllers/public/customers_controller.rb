696557203383aa713f93bbefbac42cd76d6954f9
rhama32 <r.hamaoka32915@gmail.com>
2023-03-23 06:25:24 +0000

    コメントアウト挿入

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,16 +1,20 @@
 class Public::CustomersController < ApplicationController
+  
   #会員、ゲストユーザではない場合、ログイン画面へ遷移。
   before_action :authenticate_customer!
   before_action :ensure_guest_user, only: [:edit]
   
+  #顧客詳細
   def show
     @customer = current_customer
   end
-
+　
+　#顧客編集
   def edit
     @customer = current_customer
   end
 
+　#顧客編集→アップデート
   def update
     @customer = current_customer
     if @customer.update(customer_params)
@@ -26,6 +30,7 @@ class Public::CustomersController < ApplicationController
     @customer = current_customer
   end
 
+　#顧客退会
   def withdraw
     @customer = current_customer
     @customer.update(is_deleted: true)
@@ -33,12 +38,14 @@ class Public::CustomersController < ApplicationController
       flash[:notice] = "退会処理を実行いたしました"
     redirect_to root_path
   end
-
+　
+　#ストロングパラメータ
   private
     def customer_params
       params.require(:customer).permit(:email, :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number)
     end
 
+　#ゲストユーザーの制限
   def ensure_guest_user
     @customer = current_customer
     if @customer.guest_user?
--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,14 +1,16 @@
 class Public::FavoritesController < ApplicationController
 
-  #顧客ログインしていない時は
+  #顧客ログインしていない時はログイン画面へ
   before_action :authenticate_customer!
   
+  #いいね機能
   def create
     @review = Review.find(params[:review_id])
     favorite = current_customer.favorites.new(review_id: @review.id)
     favorite.save
   end
-
+　
+　#いいね削除
   def destroy
     @review = Review.find(params[:review_id])
     favorite = current_customer.favorites.find_by(review_id: @review.id)
